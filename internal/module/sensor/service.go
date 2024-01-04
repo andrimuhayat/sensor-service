b1d4c750104d70e227608118a33ddd8392f0ffbb
Andri Muhayat <muhayatandri@gmail.com>
2024-01-04 10:05:25 +0700

    init start service sensor module

---
+++ internal/module/sensor/service.go
@@ -0,0 +1,33 @@
+package sensor
+
+import (
+	"github.com/labstack/echo/v4"
+	"sensor-service/internal/module/sensor/handler"
+	"sensor-service/internal/module/sensor/repository"
+	"sensor-service/internal/module/sensor/usecase"
+	module "sensor-service/internal/platform/common"
+	"sync"
+)
+
+func RunConsumer(wg *sync.WaitGroup, f func()) {
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		f()
+	}()
+}
+
+func StartService(dependency module.Dependency, router *echo.Echo) {
+	//init repo
+	dependency.SensorRepository = repository.NewRepository(dependency.DB)
+	//init usecase
+	dependency.SensorUseCase = usecase.NewUseCase(dependency.SensorRepository, dependency.MqttClient, dependency.GenericRepository)
+	// define handler
+	sensorHandler := handler.NewHandler(dependency.SensorUseCase)
+	//init route
+	versionRoute := router.Group("/test")
+	//run consumer mqtt
+	RunConsumer(&sync.WaitGroup{}, dependency.SensorUseCase.ListenStreamingData)
+
+	handler.NewSensorRoute(sensorHandler, versionRoute)
+}
